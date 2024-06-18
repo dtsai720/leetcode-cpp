91a5ef08ea36a073f4d442514ee2632481bc0930
david <djuni0720@gmail.com>
2024-06-18 14:15:39 +0800

    2024-06-08 done

---
+++ include/most_profit_assigning_work.h
@@ -0,0 +1,13 @@
+#ifndef MOST_PROFIT_ASSIGNING_WORK_H
+#define MOST_PROFIT_ASSIGNING_WORK_H
+
+#include <vector>
+
+namespace MostProfitAssigningWork {
+class Solution {
+   public:
+    int maxProfitAssignment(std::vector<int>& difficulty, std::vector<int>& profit, std::vector<int>& worker);
+};
+}  // namespace MostProfitAssigningWork
+
+#endif  // MOST_PROFIT_ASSIGNING_WORK_H
